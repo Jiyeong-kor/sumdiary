31a5cb9fc8ba9715e3fa62c77cc1352c06a86d04
정지영 <jeongjiyeong486@gmail.com>
2026-06-20 20:01:20 +0900

    fix(core-util): iOS 공통 날짜 포맷 컴파일 오류 수정

--- shared/core-util/src/commonMain/kotlin/com/jeong/sumdiary/core/util/DateTimeExt.kt
+++ shared/core-util/src/commonMain/kotlin/com/jeong/sumdiary/core/util/DateTimeExt.kt
@@ -3,11 +3,11 @@ package com.jeong.sumdiary.core.util
 import kotlinx.datetime.LocalDate
 import kotlinx.datetime.LocalDateTime
 import kotlinx.datetime.LocalTime
-import kotlinx.datetime.dayOfMonth
-import kotlinx.datetime.monthNumber
 
-fun LocalDate.displayText(): String = "%04d-%02d-%02d".format(year, monthNumber, dayOfMonth)
+fun LocalDate.displayText(): String = "${year.pad(4)}-${month.number.pad()}-${day.pad()}"
 
-fun LocalTime.displayText(): String = "%02d:%02d".format(hour, minute)
+fun LocalTime.displayText(): String = "${hour.pad()}:${minute.pad()}"
 
 fun LocalDateTime.toPair(): Pair<LocalDate, LocalTime> = date to time
+
+private fun Int.pad(size: Int = 2): String = toString().padStart(size, '0')
