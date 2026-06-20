791f92406b821dd588c63baf1ac720383c897c5b
정지영 <jeongjiyeong486@gmail.com>
2026-06-20 20:20:06 +0900

    fix(data): SQLDelight 생성 row 타입 사용

--- shared/data-summary/src/commonMain/kotlin/com/jeong/sumdiary/data/summary/SummaryRepositoryImpl.kt
+++ shared/data-summary/src/commonMain/kotlin/com/jeong/sumdiary/data/summary/SummaryRepositoryImpl.kt
@@ -1,7 +1,7 @@
 package com.jeong.sumdiary.data.summary
 
-import com.jeong.sumdiary.data.summary.db.SelectSummary
 import com.jeong.sumdiary.data.summary.db.SummaryDatabase
+import com.jeong.sumdiary.data.summary.db.Summary as SummaryRow
 import com.jeong.sumdiary.domain.diary.DiaryEntry
 import com.jeong.sumdiary.domain.summary.Summary
 import com.jeong.sumdiary.domain.summary.SummaryRepository
@@ -77,7 +77,7 @@ class SummaryRepositoryImpl(
         return emotions
     }
 
-    private fun SelectSummary.toSummary(): Summary = Summary(
+    private fun SummaryRow.toSummary(): Summary = Summary(
         type = SummaryType.valueOf(type),
         periodStart = LocalDate.parse(periodStart),
         periodEnd = LocalDate.parse(periodEnd),
